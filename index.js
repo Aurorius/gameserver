abec9f98cd439b05890069c6859c22c192b1057b
Aurorius <62819094+Aurorius@users.noreply.github.com>
2020-11-18 21:27:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
+  
+  socket.on('sunucuyuSelamla', () => socket.emit("clienteYolla", "slm") );
+  
+  
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
