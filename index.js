78dab88ea93edf80ada1b48549f4a832a24f4c4e
Aurorius <62819094+Aurorius@users.noreply.github.com>
2020-11-18 23:58:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -33,13 +33,13 @@ const server = express()
 
 var app = express()
  
-app.use(cors())
+.use(cors())
  
-app.get('/products/:id', function (req, res, next) {
+.get('/products/:id', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 })
  
-app.listen(PORT, function () {
+.listen(PORT, function () {
   console.log('CORS-enabled web server listening on port 80')
 })
 
