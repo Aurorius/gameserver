621a01fd45c0f6399b5183a5331d11819cbd0c4f
Aurorius <62819094+Aurorius@users.noreply.github.com>
2020-11-18 22:52:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const socketIO = require('socket.io');
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
-const server = express();
+const server = express()
 
-server.use((req, res) => res.sendFile(INDEX, { root: __dirname }));
+.get('/selam', function (req, res) {
+ res.sendFile(INDEX, { root: __dirname })
+})
 
-server.listen(PORT, () => console.log(`Listening on ${PORT}`));
+.server.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
 
