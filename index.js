9c57a67a6262cf6671bae15434f892ba2c5c4f55
Aurorius <62819094+Aurorius@users.noreply.github.com>
2020-11-18 21:32:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,4 +19,4 @@ io.on('connection', (socket) => {
   
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+////setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
