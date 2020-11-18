dbcebccf6d716caaca5fddbf6d985c531fbbe956
Aurorius <62819094+Aurorius@users.noreply.github.com>
2020-11-18 22:35:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,15 +9,17 @@ const INDEX = '/index.html';
 
 
  // .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-const server = express()
-  .use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-})
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const app = express();
+
+
+app.get('/selam', function (req, res) {
+ res.sendFile(INDEX, { root: __dirname })
+});
+
+
+
+
+ app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 
 
