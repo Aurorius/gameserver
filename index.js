52c12aeb2abf1da7e0dcfe0d0acfc90d0875b1fd
Aurorius <62819094+Aurorius@users.noreply.github.com>
2020-11-18 21:47:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,29 +2,20 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
-//.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+
 const server = express()
-  .use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
- // Add this
- if (req.method === 'OPTIONS') {
-
-      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, OPTIONS');
-      res.header('Access-Control-Max-Age', 120);
-      return res.status(200).json({});
-  }
-
-  next();
-
-})
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 
+const io = socketIO(server, {
+  cors: {
+    origin: "https://localhost:3000",
+    credentials: true
+  }
+});
 
-const io = socketIO(server);
+
 
 io.on('connection', (socket) => {
   console.log('Client connected');
