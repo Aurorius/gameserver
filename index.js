9bde720d44ea0fd10d9f55c32497f8d8422cf6cc
Aurorius <62819094+Aurorius@users.noreply.github.com>
2020-11-18 23:26:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const server = express()
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
 })
 .use('/index.html/', function (req, res, next) {
   res.sendFile(INDEX, { root: __dirname })
