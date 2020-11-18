ac7e61aaf318c7fee06b573834f4dad98111366b
Aurorius <62819094+Aurorius@users.noreply.github.com>
2020-11-18 21:59:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
