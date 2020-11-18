6202a9c6b81a3d893912628acb189e9f0b840b6b
Aurorius <62819094+Aurorius@users.noreply.github.com>
2020-11-18 22:44:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
 const express = require('express');
-
 const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-
-
- // 
-const app = express();
-
-
-//app.get('/selam', function (req, res) {
-// res.sendFile(INDEX, { root: __dirname })
-//});
-
-app.use((req, res) => res.sendFile(INDEX, { root: __dirname }));
-
-
-
- app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-
-
-
-const io = socketIO(app);
+const io = socketIO(server);
 
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
-  
-  socket.on('sunucuyuSelamla', () => socket.emit("clienteYolla", "slm") );
-  
-  
 });
-
-////setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
