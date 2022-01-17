2a83379303879257b3896b530142ed4dbead701d
Aurorius <62819094+Aurorius@users.noreply.github.com>
2022-01-17 22:38:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -34,11 +34,7 @@ const server = express()
 const whitelist = ["http://localhost:3000"]
 const corsOptions = {
   origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(null, true)
-    }
+    callback(null, true)
   },
   credentials: true,
 }
