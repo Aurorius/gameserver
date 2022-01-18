70b61b2e6cce662be74c3a7fcec1a383e236ec63
Aurorius <62819094+Aurorius@users.noreply.github.com>
2022-01-18 14:22:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const socketIO = require('socket.io');
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
+
+const server = express()
+.use(function(req, res, next) {
+  cors();
+})
+
 var io = socketIO(server, options={
  cors:true,
  origins:["/"],
