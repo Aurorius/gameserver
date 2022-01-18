067d0174f5c8d5c3b33821bbbce8b65a269bb630
Aurorius <62819094+Aurorius@users.noreply.github.com>
2022-01-18 14:36:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ const socketIO = require('socket.io');
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
-const io = socketIO(PORT);
+const io = socketIO(PORT)(httpServer, {
+  cors: {
+    origin: "http://aurorius.epizy.com/",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
 
 io.on('connection', (socket) => {
   socket.send('Hoşgeldiniz sayın ' + socket.id);
